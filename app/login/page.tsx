e887fd62037086558a728c5536aa72614c6f68e4
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-29 15:24:28 -0300

    feat: add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,11 +4,11 @@ import FormComoponent from "./_components/formComponent/formComponent";
 const LoginPage = () => {
   return (
     <section className="w-full h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center w-[900px] h-[900px] gap-10">
+      <div className="flex flex-col items-center justify-center w-full gap-10">
         <Image
           src="/logo.png"
           alt="logo image"
-          width={400}
+          width={200}
           height={200}
           className="rounded-lg object-cover"
         />
