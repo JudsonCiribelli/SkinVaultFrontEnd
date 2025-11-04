047ac7465d794cc09d59bd7ec81a4e91e175a49f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-04 15:46:44 -0300

    feat: show header component in all routes path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import HeaderComponent from "./_components/headerComponent/headerComponent";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}  antialiased`}>{children}</body>
+      <body className={`${poppins.className}  antialiased`}>
+        <HeaderComponent />
+        {children}
+      </body>
     </html>
   );
 }
