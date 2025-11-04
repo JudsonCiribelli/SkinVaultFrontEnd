b8a0479364fc10a14800a09532a31aabf00da6ce
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-04 16:39:46 -0300

    feat(auth): implement middleware for route protection

--- lib/cookiesServer.ts
+++ lib/cookiesServer.ts
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 
 export async function getCookiesServer() {
   const cookiesStorage = await cookies();
+
   const token = cookiesStorage.get("session")?.value;
 
   return token || null;
--- middleware.ts
+++ middleware.ts
@@ -1,45 +1,54 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getCookiesClient } from "./lib/cookiesClient";
-import api from "./services/api";
 
-export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-
-  if (pathname.startsWith("/_next") || pathname === "/") {
-    return NextResponse.next();
-  }
-
-  const token = await getCookiesClient();
-
-  if (pathname.startsWith("/")) {
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-
-    const isValid = await validateToken(token);
-
-    if (!isValid) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-  }
-  return NextResponse.next();
-}
+const publicRoutes = ["/signup", "/login"];
 
 async function validateToken(token: string) {
-  if (!token) {
-    return false;
-  }
+  if (!token) return false;
+  const apiBaseUrl = process.env.API_BASE_URL || "http://localhost:3333";
 
   try {
-    await api.get("/session", {
+    const response = await fetch(`${apiBaseUrl}/user`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
+      method: "GET",
     });
 
-    return true;
+    return response.ok;
   } catch (error) {
-    console.log(error);
+    console.error("Erro ao validar token:", error);
     return false;
   }
 }
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const token = req.cookies.get("session")?.value;
+
+  const isPublicRoute = publicRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (isPublicRoute) {
+    return NextResponse.next();
+  }
+
+  const signupUrl = new URL("/signup", req.url);
+
+  if (!token) {
+    console.log("Middleware: Sem token, redirecionando para /signup");
+    return NextResponse.redirect(signupUrl);
+  }
+
+  const isValid = await validateToken(token);
+
+  if (!isValid) {
+    return NextResponse.redirect(signupUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
