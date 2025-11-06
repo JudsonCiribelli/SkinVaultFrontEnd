629958b4a1415f65d7a651770d64bf3bf4eba6d0
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-06 13:43:49 -0300

    fix: get token from get token server action

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getCookiesServer } from "./lib/cookiesServer";
 
 const publicRoutes = ["/signup", "/login"];
 
 async function validateToken(token: string) {
   if (!token) return false;
+
   const apiBaseUrl = process.env.API_BASE_URL || "http://localhost:3333";
 
   try {
@@ -23,7 +25,8 @@ async function validateToken(token: string) {
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  const token = req.cookies.get("session")?.value;
+
+  const token = await getCookiesServer();
 
   const isPublicRoute = publicRoutes.some((route) =>
     pathname.startsWith(route)
@@ -34,10 +37,11 @@ export async function middleware(req: NextRequest) {
   }
 
   const signupUrl = new URL("/signup", req.url);
+  const loginUrl = new URL("/login", req.url);
 
   if (!token) {
-    console.log("Middleware: Sem token, redirecionando para /signup");
-    return NextResponse.redirect(signupUrl);
+    console.log("Middleware: Sem token, redirecionando para o login");
+    return NextResponse.redirect(loginUrl);
   }
 
   const isValid = await validateToken(token);
